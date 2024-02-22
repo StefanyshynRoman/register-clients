d7e1aeab008819adde44ae628f7f27764840b78b
roman <romanpz051@gmail.com>
2024-02-22 19:54:58 +0100

    client-service.v003form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +9,6 @@ import { ClientsModule } from './modules/clients/clients.module';
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    BrowserModule,
     CoreModule,
     HomeModule,
     AppRoutingModule,
