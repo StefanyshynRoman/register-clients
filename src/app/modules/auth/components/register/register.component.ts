e483e98b079af4d9b7550e8ed3ee098caf00a6e2
roman <romanpz051@gmail.com>
2024-02-26 10:09:05 +0100

    client-service.v006form

--- src/app/modules/auth/components/register/register.component.ts
+++ src/app/modules/auth/components/register/register.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  hide = true;
+  registerForm = new FormGroup({
+    email: new FormControl('', [
+      Validators.email,
+      Validators.minLength(5),
+      Validators.maxLength(50),
+    ]),
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', { validators: [Validators.required] }),
+  });
 
+  ngOnInit(): void {
+    console.log('');
+    this.registerForm.controls.email.disable();
+  }
+
+  onRegister() {
+    console.log(this.registerForm.value);
+  }
 }
