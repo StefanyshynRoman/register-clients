c39c50bfcd65047cf622616901570dbe8cb76518
roman <romanpz051@gmail.com>
2024-03-03 12:58:02 +0100

    client-service.v015guard

--- src/app/modules/clients/clients-routing.module.ts
+++ src/app/modules/clients/clients-routing.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ClientsComponent } from './clients.component';
-import { ClientComponent } from './components/client/client.component';
 import { ClientFormComponent } from './components/client-form/client-form.component';
+import { ClientComponent } from './components/client/client.component';
+import { clientFormDeactivateGuard } from '../core/quards/client-form-deactivate.guard';
+import { ClientResolver } from '../core/resolvers/client.resolver';
 
 const routes: Routes = [
   {
     path: '',
     component: ClientsComponent,
+    // canActivate: [authGuardActivate],
+    // resolve: { client: ClientResolver },
   },
   {
     path: 'dodaj',
     component: ClientFormComponent,
+    canDeactivate: [clientFormDeactivateGuard],
   },
   {
     path: ':id',
